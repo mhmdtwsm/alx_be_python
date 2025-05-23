b38d92f1a1888f4945c757af8f1eb4d74e35fb6c
Mhmd <sherefmohamed93@gmail.com>
2025-05-23 20:07:01 +0300

    Firt Task: basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+
+print(f"""
+Addition of {number1} and {number2} is {number1 + number2}
+Subtraction of {number1} and {number2} is {number1 - number2}
+Multiplication of {number1} and {number2} is {number1 * number2}
+""")
+
