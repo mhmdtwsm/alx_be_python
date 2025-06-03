31b603d46d741ad4c7eab40978eca17a1f41c746
Mhmd <sherefmohamed93@gmail.com>
2025-06-03 08:18:49 +0300

    Task 2. Explore `datetime` Module

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -14,5 +14,5 @@ print(f"Current date and time: {current_date}")
 
 future_date = calculate_future_date(
     int(input("Enter the number of days to add to the current date: "))
-)
+).strftime("%Y-%m-%d")
 print(f"Future date: {future_date}")
