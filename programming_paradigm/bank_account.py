5b4a9cf48a7999ce5006c1b32daf16136beb5f87
TheVHSBoy <sherefmohamed93@gmail.com>
2025-07-14 13:39:52 +0300

    Task0

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,23 +1,16 @@
 class BankAccount:
     def __init__(self, account_balance=0):
-        self.account_balance = account_balance
+        self.__account_balance = account_balance  # Private attribute
 
     def deposit(self, amount):
-        self.account_balance += amount
+        self.__account_balance += amount
 
     def withdraw(self, amount):
-        if self.account_balance != 0:
-            self.account_balance -= amount
+        if self.__account_balance >= amount:
+            self.__account_balance -= amount
             return True
         else:
             return False
 
     def display_balance(self):
-        print(self.account_balance)
-
-
-harry = BankAccount(100)
-harry.withdraw(50)
-
-
-print(harry.display_balance())
+        print(f"Current Balance: ${self.__account_balance:.2f}")
